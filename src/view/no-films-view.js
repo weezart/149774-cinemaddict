659ebf56b8b06a10ee974892fa6926770fb24ff4
weezart <weezart@yandex.ru>
2022-06-11 21:01:17 +0300

    9.4 Изменение текста заглушки.

--- src/view/no-films-view.js
+++ src/view/no-films-view.js
@@ -1,26 +1,28 @@
+import { FilterType } from '../const';
 import AbstractView from '../framework/view/abstract-view.js';
 
-const createNoFilmsTemplate = () => (
-  `<section class="films-list">
-      <h2 class="films-list__title">There are no movies in our database</h2>
+const NoFilmsTextType = {
+  [FilterType.ALL]: 'There are no movies in our database',
+  [FilterType.WATCHLIST]: 'There are no movies to watch now',
+  [FilterType.HISTORY]: 'There are no watched movies now',
+  [FilterType.FAVORITES]: 'There are no favorite movies now',
+};
 
-      <!--
-        Значение отображаемого текста зависит от выбранного фильтра:
-          * All movies – 'There are no movies in our database'
-          * Watchlist — 'There are no movies to watch now';
-          * History — 'There are no watched movies now';
-          * Favorites — 'There are no favorite movies now'.
-      -->
-    </section>
-  `
-);
+const createNoFilmsTemplate = (filterType) => {
+  const noFilmsTextValue = NoFilmsTextType[filterType];
+
+  return `<section class="films-list"><h2 class="films-list__title">${noFilmsTextValue}</h2></section>`;
+};
 
 export default class createNoFilmsView extends AbstractView {
-  constructor() {
+  #filterType = null;
+
+  constructor (filterType) {
     super();
+    this.#filterType = filterType;
   }
 
   get template() {
-    return createNoFilmsTemplate();
+    return createNoFilmsTemplate(this.#filterType);
   }
 }
