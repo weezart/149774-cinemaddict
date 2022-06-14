a571f440788ff9d8e3fde3eb447b5f8c70de1993
weezart <weezart@yandex.ru>
2022-06-14 21:58:36 +0300

    8.6 Добавит модуль FilmsApiService в точке входа. WIP

--- src/main.js
+++ src/main.js
@@ -6,13 +6,17 @@ import FilterPresenter from './presenter/filter-presenter.js';
 import FilmsModel from './model/film-model.js';
 import CommentsModel from './model/comment-model.js';
 import FilterModel from './model/filter-model.js';
+import FilmsApiService from './films-api-service.js';
+
+const AUTHORIZATION = 'Basic hXS2sgf404s4d4gwl1sd2wj';
+const END_POINT = 'https://17.ecmascript.pages.academy/cinemaddict';
 
 const siteBodyElement = document.querySelector('body');
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = document.querySelector('.header');
 
 const footerStatsElement = document.querySelector('.footer__statistics');
-const filmsModel = new FilmsModel();
+const filmsModel = new FilmsModel(new FilmsApiService(END_POINT, AUTHORIZATION));
 const commentsModel = new CommentsModel();
 const filterModel = new FilterModel();
 const boardPresenter = new BoardPresenter(siteMainElement, siteBodyElement, filmsModel, commentsModel, filterModel);
--- src/model/film-model.js
+++ src/model/film-model.js
@@ -2,7 +2,21 @@ import Observable from '../framework/observable.js';
 import { generateFilm } from '../mock/film.js';
 
 export default class FilmsModel extends Observable {
+  #filmsApiService = null;
   #films = Array.from({length: 6}, generateFilm);
+
+  constructor(filmsApiService) {
+    super();
+    this.#filmsApiService = filmsApiService;
+
+    this.#filmsApiService.films.then((films) => {
+      console.log(films);
+      // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
+      // а ещё на сервере используется snake_case, а у нас camelCase.
+      // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
+      // Есть вариант получше - паттерн "Адаптер"
+    });
+  }
 
   get films () {
     return this.#films;
